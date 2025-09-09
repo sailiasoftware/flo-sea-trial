d92c118f4f5ea3e4b521b624d56c878f6609e481
Flo Ragossnig <flo@codeversity.com>
2025-09-09 11:39:52 +0100

    Basic theme and landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,50 @@
 import "@/styles/globals.css";
 
-import { type Metadata } from "next";
-
-import { TRPCReactProvider } from "@/trpc/react";
+import type { Metadata } from "next";
+import Link from "next/link";
+import { ModeToggle } from "@/components/custom";
 import { ThemeProvider } from "@/providers";
+import { TRPCReactProvider } from "@/trpc/react";
 
 export const metadata: Metadata = {
-  title: "Tack & Co Sailing School",
-  description: "A simple booking system for getting the guys to the next level",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+	title: "Tack & Co Sailing School",
+	description: "A simple booking system for getting the guys to the next level",
+	icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en">
-      <body>
-        <ThemeProvider
-						attribute="class"
-						defaultTheme="system"
-						enableSystem
-						disableTransitionOnChange
-					>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<TRPCReactProvider>
+						<nav className="flex h-16 w-full items-center justify-between px-4">
+							<div className="flex gap-4">
+								<Link href="/" className="hover:text-primary">
+									Home
+								</Link>
+								<Link href="/book" className="hover:text-primary">
+									Book
+								</Link>
+								<Link href="/admin" className="hover:text-primary">
+									Admin
+								</Link>
+							</div>
+							<div>
+								<ModeToggle />
+							</div>
+						</nav>
+						{children}
+					</TRPCReactProvider>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
---
+++ src/components/custom/index.ts
@@ -0,0 +1,2 @@
+export {ModeToggle} from './modeToggle'
+export {SailboatIcon} from './sailboatIcon'
